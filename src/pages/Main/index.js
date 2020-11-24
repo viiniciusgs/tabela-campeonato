dc91c812bbf0d1fcff820698245fa501195971f7
Vinicius Gabriel <viiniciusgs03@gmail.com>
2020-11-24 18:39:19 -0300

    Finalizando apresentaçao da pagina

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,24 +2,43 @@ import React from 'react';
 
 import api from '../../services/api';
 
+import { IoIosArrowDown } from 'react-icons/io';
+
+import imageIcon from '../../images/rocket-league.svg';
 import './styles.css';
+import './animations.css';
 
 export default function Main() {
     return(
         <div className="main">
-            <div className="players">
-                <div className="player">
-                    ExFury
+            <div className="container">
+                <h1>CAMPEONATO PÃO DE BATATÃ</h1>
+
+                <div className="cards">
+                    <div className="player">
+                        <img src={imageIcon}></img>
+                        ExFury
+                    </div>
+
+                    <div className="player">
+                        <img src={imageIcon}></img>
+                        GabrielS3T
+                    </div>
+
+                    <div className="player">
+                        <img src={imageIcon}></img>
+                        Neon
+                    </div>
+
+                    <div className="player">
+                        <img src={imageIcon}></img>
+                        Zoltan
+                    </div>
                 </div>
-                <div className="player">
-                    GabrielS3T
-                </div>
-                <div className="player">
-                    Neon
-                </div>
-                <div className="player">
-                    ZoltanBR
-                </div>
+
+                <button>
+                    <IoIosArrowDown size={20} />
+                </button>
             </div>
         </div>
     );
