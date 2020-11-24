49768d8a1a837ebd354a8485a4affdcd296e34b5
Vinicius Gabriel <viiniciusgs03@gmail.com>
2020-11-24 20:25:44 -0300

    Tabela de classificaçao

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import api from '../../services/api';
 
 import { IoIosArrowDown } from 'react-icons/io';
 
 import imageIcon from '../../images/rocket-league.svg';
+import rankGold from '../../images/gold.png';
+import rankSilver from '../../images/silver.png';
+import rankBronze from '../../images/bronze.png';
+import unranked from '../../images/unranked.png';
 import './styles.css';
 import './animations.css';
 
 export default function Main() {
+    const [players, setPlayers] = useState([]);
+
+    useEffect(() => {
+        loadPlayers();
+    }, []);
+
+    async function loadPlayers() {
+        const response = await api.get('/players');
+
+        setPlayers(response.data);
+    }
+
     return(
         <div className="main">
             <div className="container">
@@ -37,12 +53,55 @@ export default function Main() {
                 </div>
 
                 <button>
-                    <IoIosArrowDown size={20} />
+                    <a href="#championship-infos">
+                        <IoIosArrowDown size={20} />
+                    </a>
                 </button>
             </div>
 
-            <div className="championship-infos">
+            <div id="championship-infos" className="championship-infos">
+                <table className="content-table">
+                    <thead>
+                        <tr>
+                            <th>CLASSIFICAÇÃO</th>
+                            <th>P</th>
+                            <th>J</th>
+                            <th>V</th>
+                            <th>D</th>
+                            <th>GP</th>
+                            <th>GC</th>
+                            <th>SG</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {players.map((player, index) => {
+                            let rank = '';
 
+                            if(index === 0) {
+                                rank = rankGold; 
+                            } else if(index === 1) {
+                                rank = rankSilver;
+                            } else if(index === 2) {
+                                rank = rankBronze;
+                            } else {
+                                rank = unranked;
+                            }
+
+                            return(
+                                <tr key={player._id}>
+                                    <td><img src={rank} />{` ${player.name}`}</td>
+                                    <td>{player.score}</td>
+                                    <td>{player.gamesPlayed}</td>
+                                    <td>{player.wins}</td>
+                                    <td>{player.loses}</td>
+                                    <td>{player.goalsScored}</td>
+                                    <td>{player.concededGoals}</td>
+                                    <td>{player.goalDifference}</td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
             </div>
         </div>
     );
